82c7bfd866128341346db9e082d049c6fee92993
Joffrey Schoemacker <1002websolutions@gmail.com>
2022-04-03 18:19:10 +0200

    3rd commit

--- Second.js
+++ Second.js
@@ -1,38 +1,95 @@
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
-import React, { useState } from 'react'
+import { View, Text, Button, TextInput, StyleSheet, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import uuid from 'react-native-uuid';
 
 const Second = (props) => {
 
-  const [content, setContent] = useState({});
-  const [list, setList] = useState('');
+  
+  const [content, setContent] = useState('');
+  const [list, setList] = useState([]);
 function handleChange(text) {setContent(text)} 
-const send = () => {setList(content)} 
+const send = () => {setList((currentCourseGoals) => [...currentCourseGoals, { text: content, key: uuid.v4()},])} 
+
   return (
-      <>
-    <View style={styles.first}>
-      <Text>Second</Text>
-    <TextInput placeholder='Votre texte' onChangeText={handleChange}/>
-      <Button title="Valider" onPress={send}/>      
-      <Text>{list}</Text>
+    
+      <View style={styles.first}>  
+      <Text style={styles.main}>Ma todo liste</Text>
+    <View style={styles.second}>
+    
+    <TextInput placeholder='Votre tâche ?' onChangeText={handleChange} style={styles.textInput}/>
+    <View style={styles.button}> 
+      <Button title="Valider" onPress={send}/>     
       </View>
+      
+      </View>
+  <View style={styles.trois}>    
+  <FlatList data={list} renderItem={itemData => {
+    itemData.index
+    return(
+      <View style={styles.textGoal}>
+        <Text style={styles.textColor}>{itemData.item.text}</Text>
+      </View>
+    )
+  }} 
+    keyExtractor={(item) => {
+      return item.id
+    }}
+  />
+      </View>   
     <View>
 
     </View>
-    </>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  main: {
+    fontSize: 22,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: '700'
+  },
   first: {
-    flex: 1,
+     flex: 1,
+     backgroundColor: 'white',
+     padding: 25,
+  },  
+    second: {
+    flex: 1,   
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+  },
+  textInput: {
     justifyContent: 'center',
-    alignContent: 'center'
+    width: '70%',
+    marginVertical: 30,
+    padding: 8,
+    marginRight: 2,
+    backgroundColor: '#ececec'
   },
-  second: {
-    flex: 2,
+  button: {
+    alignContent: 'center',
+    justifyContent: 'center',
   },
+
   trois: {
-    flex: 3
+    paddingTop: 20,
+    borderTopColor: '#C8C8C8',
+    borderTopWidth: 1,
+    flex: 5
+  },
+  textGoal: {
+ margin: 8,
+ padding: 8,
+ borderRadius: 6,
+ backgroundColor: '#5e0acc',
+  },
+  textColor: {
+    color: 'white',
+    paddingHorizontal: 10
   }
  })
 
